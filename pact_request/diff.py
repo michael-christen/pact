0696baff80711d408f4a889fcc02f0617125019d
karatekid <mchristen96@gmail.com>
2017-06-10 19:12:01 -0700

    Got diff tree working

--- pact_request/diff.py
+++ pact_request/diff.py
@@ -1,29 +1,66 @@
-from itertools import izip_longest
 import urllib2
 
 
-# class Difference(object):
-#     def __init__(self, expected, actual):
-#         # Ensure there actually is a difference
-#         assert expected != actual
-#         self.expected = expected
-#         self.actual = actual
-#
-#     def __unicode__(self):
-#         return u'Expected {expected}, but got {actual}'.format(
-#             self.expected, self.actual)
+class Difference(object):
+    def __init__(self, expected, actual):
+        # Ensure there actually is a difference
+        assert expected != actual
+        self.expected = expected
+        self.actual = actual
+
+    def __unicode__(self):
+        return u'Expected {expected}, but got {actual}'.format(
+            self.expected, self.actual)
+
+
+# Singleton
+ABSENT = object()
+
+
+class UnexpectedActual(Difference):
+    def __init__(self, actual):
+        super(UnexpectedActual, self).__init__(ABSENT, actual)
+
+
+class ActualNotFound(Difference):
+    def __init__(self, expected):
+        super(ActualNotFound, self).__init__(expected, ABSENT)
+
+
+# Actual differences used
+class UnexpectedIndex(UnexpectedActual):
+    pass
+
+
+class UnexpectedKey(UnexpectedActual):
+    pass
+
+
+class IndexNotFound(ActualNotFound):
+    pass
+
+
+class KeyNotFound(ActualNotFound):
+    pass
 
 
 def diff_hash_with_rules(expected, actual, key_rules):
     assert isinstance(expected, dict)
     assert isinstance(actual, dict)
-    diffs = []
+    diff_tree = {}
+    received_diff = False
     for expected_k, expected_v in expected.iteritems():
         rules = key_rules.get(expected_k, {})
         diff_engine = PactDiffEngine(**rules)
         actual_v = actual[expected_k]
-        diffs.extend(diff_engine.diff_val(expected_v, actual_v))
-    return diffs
+        diff_tree[expected_k] = diff_engine.diff_val(expected_v, actual_v)
+        if diff_engine.diff_received:
+            received_diff = True
+    if received_diff:
+        print diff_tree
+        return diff_tree
+    else:
+        return {}
 
 
 class PactDiffEngine(object):
@@ -34,10 +71,13 @@ class PactDiffEngine(object):
         self.allow_unexpected_keys = allow_unexpected_keys
         self.ignore_value_whitespace = ignore_value_whitespace
         self.case_insensitive_keys = case_insensitive_keys
+        # Keep track of whether a diff exists
+        self.diff_received = False
 
     def diff_val(self, expected, actual):
         if type(expected) != type(actual):
-            return ['{} mismatch type {}'.format(expected, actual)]
+            self.diff_received = True
+            return Difference(expected, actual)
         # Input cleaning
         if isinstance(expected, basestring):
             if self.ignore_value_whitespace:
@@ -51,16 +91,25 @@ class PactDiffEngine(object):
         elif isinstance(expected, (list, tuple)):
             return self.diff_list(expected, actual)
         elif expected != actual:
-            return ['{} not equal {}'.format(expected, actual)]
+            self.diff_received = True
+            return Difference(expected, actual)
         else:
-            return []
+            return expected
 
     def diff_list(self, expected, actual):
         diffs = []
-        for expected_v, actual_v in izip_longest(expected, actual):
-            diffs.extend(self.diff_val(expected_v, actual_v))
-        if len(expected) != len(actual):
-            diffs.append("None value at end of list")
+        for i, expected_v in enumerate(expected):
+            try:
+                actual_v = actual[i]
+            except IndexError:
+                self.diff_received = True
+                diffs.append(IndexNotFound(expected_v))
+            else:
+                diffs.append(self.diff_val(expected_v, actual_v))
+        if len(actual) > len(expected):
+            for actual_v in actual[len(expected):]:
+                self.diff_received = True
+                diffs.append(UnexpectedIndex(actual_v))
         return diffs
 
     def diff_hash(self, expected, actual):
@@ -73,16 +122,17 @@ class PactDiffEngine(object):
         if self.case_insensitive_keys:
             actual = {k.lower(): v for k, v in actual.iteritems()}
             expected = {k.lower(): v for k, v in expected.iteritems()}
-        diffs = []
-        if not self.allow_unexpected_keys and (
-                set(expected.keys()) != set(actual.keys())):
-            diffs.append("unexpected keys {}, {}".format(
-                actual.keys(), expected.keys()))
+        diffs = {}
         for expected_k, expected_v in expected.iteritems():
             try:
                 actual_v = actual[expected_k]
             except KeyError:
-                diffs.append("Key not found <{}>".format(expected_k))
+                self.diff_received = True
+                diffs[expected_k] = KeyNotFound(expected_v)
             else:
-                diffs.extend(self.diff_val(expected_v, actual_v))
+                diffs[expected_k] = self.diff_val(expected_v, actual_v)
+        for actual_k, actual_v in actual.iteritems():
+            if actual_k not in expected and not self.allow_unexpected_keys:
+                self.diff_received = True
+                diffs[actual_k] = UnexpectedKey(actual_v)
         return diffs
--- pact_request/tests/test_basic.py
+++ pact_request/tests/test_basic.py
@@ -30,7 +30,7 @@ class TestSpec1(TestCase):
         (spec.test_string(), spec.content)
         for spec in get_all_json_specs(version='1')])
     def test_pact_spec(self, name, spec_content):
-        print unicode(spec_content)
+        # print unicode(spec_content)
         diffs = spec_content.expected.diff(spec_content.actual)
         if spec_content.match:
             self.assertEqual(len(diffs), 0)
